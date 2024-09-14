aa71c14362e5d0a0b3e4d9cf4e69cac325536849
HenokYohanes <henokyohanesdev@gmail.com>
2024-09-13 21:26:51 -0500

    project initialized and components created.

--- react-netflix-clone/src/App.jsx
+++ react-netflix-clone/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Home from './Pages/Home/Home'
 import './App.css'
 
 export default function App() {
 
   return (
     <>
-      
+      <Home />
     </>
   )
 }
